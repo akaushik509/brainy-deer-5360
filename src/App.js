91fd7dbf5df0cd1f811e6f23c7344a95b680a425
vaishnavi borkar <vaishnaviborkar29@gmail.com>
2023-02-22 21:40:02 +0530

    day2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import AllRoutes from "./Routes/AllRoutes";
-import Register from "./Pages/Register";
 
 function App() {
   return (
     <div className="App">
       <AllRoutes />
-      <Register />
     </div>
   );
 }
--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../Pages/Home";
-
+import Register from "../Pages/Register";
 
 function AllRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}></Route>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/login" element={<Register />} />
     </Routes>
   );
 }
