3bef526bba50e169caf0ff918f1d16a310670c1a
vaishnavi borkar <vaishnaviborkar29@gmail.com>
2023-02-24 00:23:16 +0530

    day-3 added

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../Pages/Home";
+import Login from "../Pages/Login";
 import Register from "../Pages/Register";
 
 function AllRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
-      <Route path="/login" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
