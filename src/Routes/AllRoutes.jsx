98b937415149a0270db421f577a4eb89acca0a7e
avinash <akaushik509@gmail.com>
2023-02-25 23:38:39 +0530

    day5

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -3,19 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import Cart from "../Pages/Cart/Cart";
 import Home from "../Pages/Home";
 import MensPage from "../Pages/MensPage";
-import MyAcoount from "../Pages/MyAcoount";
+import MyAccount from "../Pages/MyAccount";
+import Order from "../Pages/Order";
 import Register from "../Pages/Register";
 
 function AllRoutes() {
   return (
     <Routes>
-
       <Route path="/" element={<Home/>}></Route>
       <Route path="/cart" element={<Cart/>}></Route>
       <Route path="/login" element={<Register />} />
       <Route path="/mens" element={<MensPage/>} />
-      <Route path="/myaccount" element={<MyAcoount/>} />
-
+      <Route path="/myaccount" element={<MyAccount/>} />
+      <Route path="/order" element={<Order/>}></Route>
     </Routes>
   );
 }
