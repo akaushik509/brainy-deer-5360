6bc41acf66149cd954f2eaea37e1fb2a324dd346
vaishnavi borkar <vaishnaviborkar29@gmail.com>
2023-02-25 23:13:02 +0530

    day5

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -13,87 +13,133 @@ import {
   Text,
   useColorModeValue,
   Link,
+  Image,
+  Container,
 } from "@chakra-ui/react";
-import { useState } from "react";
+
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Appcontext } from "../context/Appcontext";
+
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 
 function Register() {
   const [showPassword, setShowPassword] = useState(false);
+  const [user, setUser] = useState({});
+  const navigate = useNavigate();
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const { handleSignup } = useContext(Appcontext);
   return (
-    <Flex
-      minH={"100vh"}
-      align={"center"}
-      justify={"center"}
-      bg={useColorModeValue("gray.50", "gray.800")}
-    >
-      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-        <Stack align={"center"}>
-          <Heading fontSize={"4xl"} textAlign={"center"}>
-            Sign up
-          </Heading>
-          <Text fontSize={"lg"} color={"gray.600"}>
-            to enjoy all of our cool features ✌️
-          </Text>
-        </Stack>
-        <Box
-          rounded={"lg"}
-          bg={useColorModeValue("white", "gray.700")}
-          boxShadow={"lg"}
-          p={8}
+    <Container maxW={"5xl"}>
+      <HStack>
+        <Box flex={1}>
+          <Image src="https://images.bewakoof.com/web/desktop-sign-up-banner--1623760676.png" />
+        </Box>
+        <Flex
+          minH={"100vh"}
+          align={"center"}
+          justify={"center"}
+          bg={useColorModeValue("gray.50", "gray.800")}
         >
-          <Stack spacing={4}>
-            <FormControl id="firstName" isRequired>
-              <FormLabel>First Name</FormLabel>
-              <Input type="text" />
-            </FormControl>
-
-            <FormControl id="email" isRequired>
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" />
-            </FormControl>
-            <FormControl id="password" isRequired>
-              <FormLabel>Password</FormLabel>
-              <InputGroup>
-                <Input type={showPassword ? "text" : "password"} />
-                <InputRightElement h={"full"}>
-                  <Button
-                    variant={"ghost"}
-                    onClick={() =>
-                      setShowPassword((showPassword) => !showPassword)
-                    }
-                  >
-                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-            </FormControl>
-            <FormControl id="imageurl">
-              <FormLabel>Image</FormLabel>
-              <Input type="imageurl" />
-            </FormControl>
-            <Stack spacing={10} pt={2}>
-              <Button
-                loadingText="Submitting"
-                size="lg"
-                bg={"blue.400"}
-                color={"white"}
-                _hover={{
-                  bg: "blue.500",
-                }}
-              >
+          <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+            <Stack align={"center"}>
+              <Heading fontSize={"4xl"} textAlign={"center"}>
                 Sign up
-              </Button>
-            </Stack>
-            <Stack pt={6}>
-              <Text align={"center"}>
-                Already a user? <Link color={"blue.400"}>Login</Link>
+              </Heading>
+              <Text fontSize={"2xl"} color={"#f09e06"}>
+                Hi new buddy, let's get you started with theTrendy Treasures!
               </Text>
             </Stack>
+            <form onSubmit={(e) => handleSignup(e, user)}>
+              <Box
+                rounded={"lg"}
+                bg={useColorModeValue("white", "yellow.700")}
+                boxShadow={"lg"}
+                p={8}
+              >
+                <Stack spacing={4}>
+                  <Flex>
+                    <Box>
+                      <FormControl id="firstName" isRequired>
+                        <FormLabel>First Name</FormLabel>
+                        <Input
+                          onChange={handleChange}
+                          name="name"
+                          type="text"
+                        />
+                      </FormControl>
+                    </Box>
+                    <Box>
+                      <FormControl id="imageurl">
+                        <FormLabel>Image</FormLabel>
+                        <Input
+                          onChange={handleChange}
+                          name="imageurl"
+                          type="imageurl"
+                        />
+                      </FormControl>
+                    </Box>
+                  </Flex>
+                  <FormControl id="email" isRequired>
+                    <FormLabel>Email address</FormLabel>
+                    <Input onChange={handleChange} name="email" type="email" />
+                  </FormControl>
+                  <FormControl id="password" isRequired>
+                    <FormLabel>Password</FormLabel>
+                    <InputGroup>
+                      <Input
+                        onChange={handleChange}
+                        name="password"
+                        type={showPassword ? "text" : "password"}
+                      />
+                      <InputRightElement h={"full"}>
+                        <Button
+                          variant={"ghost"}
+                          onClick={() =>
+                            setShowPassword((showPassword) => !showPassword)
+                          }
+                        >
+                          {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                        </Button>
+                      </InputRightElement>
+                    </InputGroup>
+                  </FormControl>
+
+                  <Stack spacing={10} pt={2}>
+                    <Button
+                      type="submit"
+                      loadingText="Submitting"
+                      size="lg"
+                      bg={"yellow.400"}
+                      color={"white"}
+                      _hover={{
+                        bgImage: "linear-gradient(to right, #58aa50 ,#f09e06 )",
+                      }}
+                      bgImage={"linear-gradient(to right, #f09e06 , #fc490b )"}
+                    >
+                      Sign up
+                    </Button>
+                  </Stack>
+                  <Stack pt={6}>
+                    <Text align={"center"}>
+                      Already a user?{" "}
+                      <Link color={"blue.400"} href="login">
+                        Login
+                      </Link>
+                    </Text>
+                  </Stack>
+                </Stack>
+              </Box>
+            </form>
           </Stack>
-        </Box>
-      </Stack>
-    </Flex>
+        </Flex>
+      </HStack>
+    </Container>
   );
 }
 export default Register;
--- src/context/Appcontext.jsx
+++ src/context/Appcontext.jsx
@@ -1,11 +1,11 @@
 import React, { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { useToast } from "@chakra-ui/react";
 export const Appcontext = createContext();
 
 const AppcontextProvider = ({ children }) => {
   const [isAuth, setIsAuth] = useState(localStorage.getItem("token") || false);
-
+  const toast = useToast();
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
 
@@ -26,10 +26,22 @@ const AppcontextProvider = ({ children }) => {
           console.log(data);
           if (data.msg === "Logged In") {
             localStorage.setItem("token", data.token);
-            navigate("/");
+            toast({
+              description: "Signed in successfully",
+              status: "success",
+              duration: 2000,
+              isClosable: true,
+            });
+            navigate("/", { replace: true });
           } else if (data.msg === "Wrong Credentials") {
             alert(data.msg);
             console.log(data.msg);
+            toast({
+              description: "Failed",
+              status: "error",
+              duration: 2000,
+              isClosable: true,
+            });
             navigate("/login");
           }
         })
@@ -42,8 +54,8 @@ const AppcontextProvider = ({ children }) => {
   const handleSignup = (e, user) => {
     e.preventDefault();
 
-    let { name, email, password, imageUrl } = user;
-    if (name && email && password && imageUrl) {
+    let { name, email, password } = user;
+    if (name && email && password) {
       fetch("https://grumpy-lingerie-foal.cyclic.app/users/register", {
         method: "POST",
         headers: {
@@ -53,9 +65,17 @@ const AppcontextProvider = ({ children }) => {
       })
         .then((res) => res.json())
         .then((data) => {
-          console.log(data);
+          // console.log(data);
           if (data.msg === "New User has been registered.") {
-            navigate("/login");
+            toast({
+              title: "Account created.",
+              description: "Your account has been successfully created",
+              status: "success",
+              duration: 5000,
+              isClosable: true,
+            });
+            navigate("/login", { replace: true });
+            // window.location.href = "/login";
             //   } else if (data.msg === "Please login, user already exist") {
             //     alert(data.msg);
             //     navigate("/login");
@@ -63,7 +83,13 @@ const AppcontextProvider = ({ children }) => {
         })
         .catch((err) => console.log(err));
     } else {
-      alert("Please provide all fields.");
+      toast({
+        description: "Please provide all fields",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+      // alert("Please provide all fields.");
     }
   };
 
